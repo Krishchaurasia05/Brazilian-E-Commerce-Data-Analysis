95cf7d9c5993aeeec97b808bd9e5baba8582292c
Krish Chaurasia <chaurasiakrish10@gmail.com>
2026-07-25 13:42:20 +0530

    used open api

--- src/api_data_extract.py
+++ src/api_data_extract.py
@@ -1,1 +1,13 @@
+import requests
+import os
+import dotenv
 
+dotenv.load_dotenv()
+data = {}
+try:
+    req = requests.get('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json')
+    if req.status_code == 200:
+            data.update(req.json())
+    print(data['usd'])
+except Exception as e:
+    print('error',e)
